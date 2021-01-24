8f3e95adb73741c61e3bc411c5b7b590e63ac33b
Isaac Trimble-Pederson <wtpisaac@icloud.com>
2021-01-24 15:56:31 -0800

    Removed template cruft

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,12 +7,10 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default defineComponent({
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  name: "Home"
+  // components: {
+  // }
 });
 </script>

0e581e9964ce4e4330d87bad971345c5eb36f013
Isaac Trimble-Pederson <wtpisaac@icloud.com>
2021-01-25 13:16:40 -0800

    Final removal of Vue template items

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,5 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <div class="home"></div>
 </template>
 
 <script lang="ts">

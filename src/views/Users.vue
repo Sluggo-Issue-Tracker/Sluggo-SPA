f49c149430759e2e572f8d4a40a46b6c0ca60cff
Isaac Trimble-Pederson <wtpisaac@icloud.com>
2021-01-26 21:53:15 -0800

    Ported HTML of Users page

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,6 +1,45 @@
 <template>
-  <div id="users-page">
-    <h1>Users</h1>
+  <div class="section" id="vue-users">
+    <div class="container">
+      <div class="section columns">
+        <div class="column is-one-third ">
+          <p class="title">Team Members</p>
+        </div>
+        <div class="column">
+          <div class="field is-pulled-right">
+            <div class="control has-icons-left">
+              <span class="icon is-medium">
+                <i class="fa fa-search"></i>
+              </span>
+              <input class="input" type="text" placeholder="Search Users" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <br />
+      <div class="columns is-multiline">
+        <!-- Member Selection Panel -->
+        <div class="column is-one-third">
+          <div class="box has-background-grey-lighter">
+            <article class="media">
+              <div class="media-left">
+                <figure class="image is-96x96">
+                  <img alt="Image" />
+                </figure>
+              </div>
+              <div class="media-content">
+                <div class="content">
+                  <p>
+                    <strong class="is-4">USER NAME</strong>
+                    <br />
+                  </p>
+                </div>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 

24e9b9fca16c93de8e40e564b6285f23726d771b
Samuel <sadschmi@ucsc.edu>
2021-03-16 19:19:27 -0700

    oversight

--- src/methods/ticketPage.ts
+++
@@ -1,5 +0,0 @@
-import { TicketRecord } from "@/api/tickets";
-
-export function generateTicketPageLink(teamId: number, ticketId?: number) {
-    return `/teams/${team.id}` + (typeof loc !== "undefined" ? `/${loc}/` : ``);
-}

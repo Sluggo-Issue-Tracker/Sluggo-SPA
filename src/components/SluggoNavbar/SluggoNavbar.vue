e50b4e469a7d33c6bc5bcebc73c0c0f52ad97709
Samuel Schmidt <tdimhcsleumas@gmail.com>
2021-07-23 00:17:10 -0700

    added initial sluggo icon

--- src/components/SluggoNavbar/SluggoNavbar.vue
+++ src/components/SluggoNavbar/SluggoNavbar.vue
@@ -1,20 +1,31 @@
 <template>
-  <nav class="navbar" role="navigation" aria-label="main navigation">
-    hello world!
+  <nav
+    class="navbar navbarContainer"
+    role="navigation"
+    aria-label="main navigation"
+  >
+    <IconSluggo :height="50" :width="50" />
   </nav>
-  <div class="">
+  <div class="navigationContainer">
+    <div class="sidebarMenu">
+      <p>
+        Hello world!
+      </p>
+    </div>
     <router-view />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import IconSluggo from "@/assets/IconSluggo/IconSluggo.vue";
 
 const sluggoNavbarComponent = defineComponent({
-  name: "SluggoNavbar"
+  name: "SluggoNavbar",
+  components: {IconSluggo}
 });
 
 export default sluggoNavbarComponent;
 </script>
 
-<style scoped src="./styles.module.scss"></style>
+<style scoped src="./styles.module.scss" lang="scss"></style>

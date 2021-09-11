106feaf50217675f7852ef600c1b5adebf5b017a
Samuel Schmidt <tdimhcsleumas@gmail.com>
2021-09-11 16:29:15 -0700

    more styling for tickets

--- src/views/Home/components/TicketRow.vue
+++ src/views/Home/components/TicketRow.vue
@@ -3,26 +3,28 @@
     class="rowContainer"
     :style="{ backgroundColor: ticket.status?.color ?? '#EEEEEE' }"
   >
-    <div class="cell">
+    <div class="iconCell">{{ ticket.status?.title ?? "No Status" }}</div>
+    <div class="titleCell">
       {{ ticket.title }}
     </div>
-    <div class="cell">
+    <div class="cell tagContainer">
       <Tag v-for="tag in ticket.tag_list" :tag="tag" :key="tag.object_uuid" />
     </div>
-    <div class="cell">{{ ticket.created }}</div>
-    <div class="cell">{{ ticket.status?.title ?? "No Status" }}</div>
-    <div class="cell">
-      <ProfileEmblem :name="ticket.assigned_user?.username ?? 'Unassigned'" />
-      <span>{{ ticket.assigned_user?.username ?? "Unassigned" }}</span>
+    <div class="iconCell">
+      <ProfileEmblem
+        :name="ticket.assigned_user?.owner.username ?? 'Unassigned'"
+      />
+      <span>{{ ticket.assigned_user?.owner.username ?? "Unassigned" }}</span>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType, ref } from "vue";
+import { computed, defineComponent, PropType } from "vue";
 import ProfileEmblem from "@/components/ProfileEmblem";
 import Tag from "@/components/Tag";
 import { ReadTicketRecord } from "@/api/types";
+import { DateTime } from "luxon";
 
 export default defineComponent({
   name: "TicketRow",

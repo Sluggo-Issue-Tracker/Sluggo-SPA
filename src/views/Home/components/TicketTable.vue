8f60a1ff712f77ba20119c790b7348cffff5ee76
Samuel Schmidt <tdimhcsleumas@gmail.com>
2021-09-14 14:50:28 -0700

    fix minor pagination issue

--- src/views/Home/components/TicketTable.vue
+++ src/views/Home/components/TicketTable.vue
@@ -8,17 +8,17 @@
     </div>
     <div v-else-if="data" class="dataContainer">
       <TicketRow v-for="ticket in data" :key="ticket.id" :ticket="ticket" />
+      <PaginationControls
+        :current-page="page"
+        :total-pages="totalPages"
+        @updatePage="handlePageUpdate"
+      />
     </div>
-    <PaginationControls
-      :current-page="page"
-      :total-pages="200"
-      @updatePage="handlePageUpdate"
-    />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType, ref } from "vue";
+import { computed, defineComponent, PropType, ref } from "vue";
 import { ReadTicketRecord } from "@/api/types";
 import { AxiosError } from "axios";
 import { QueryState } from "@/methods/wrapExceptions";
@@ -43,10 +43,23 @@ export default defineComponent({
     // this may seem weird, but since children of the
     // query object are what change, not the queryState itself
     // this component does not rerender
-    const loading = ref(props.queryState.loading);
-    const data = ref(props.queryState.data);
-    const error = ref(props.queryState.error);
+    const pageSize = 5;
     const page = ref(1);
+    const loading = ref(props.queryState.loading);
+    const data = computed(() => {
+      if (props.queryState.data.value) {
+        return props.queryState.data.value.slice(page.value - 1, pageSize);
+      }
+      return undefined;
+    });
+    const error = ref(props.queryState.error);
+
+    const totalPages = computed(() => {
+      if (props.queryState.data.value) {
+        return Math.ceil(props.queryState.data.value.length / pageSize);
+      }
+      return 1;
+    });
     const handlePageUpdate = (newPageNumber: number) => {
       page.value = newPageNumber;
     };
@@ -56,6 +69,7 @@ export default defineComponent({
       data,
       error,
       handlePageUpdate,
+      totalPages,
       page
     };
   }

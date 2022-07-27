4b75f9c4e6bfc2caba37f3f89b57d0754acb4f7f
tdimhcsleumas <56523092+tdimhcsleumas@users.noreply.github.com>
2022-07-27 00:24:18 -0700

    Revert back to original proxy

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,20 @@
 module.exports = {
   devServer: {
     proxy: {
-      "^/sluggo": {
-        target: "http://127.0.0.1:80/",
+      "^/api": {
+        target: "http://127.0.0.1:8000/",
         ws: true,
         secure: false,
         changeOrigin: true
         // pathRewrite: {
         //   '^/api': ''
         // }
+      },
+      "^/auth": {
+        target: "http://127.0.0.1:8000/",
+        ws: true,
+        secure: false,
+        changeOrigin: true
       }
     }
   }

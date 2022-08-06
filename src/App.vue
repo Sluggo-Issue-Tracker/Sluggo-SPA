a3566b6660b16fbb0c831553f0b6fd360f0e1134
Samuel Schmidt <tdimhcsleumas@gmail.com>
2022-08-05 23:18:07 -0700

    move footer to own component

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,7 @@
     <section class="section has-background-light is-fullheight">
       <router-view />
     </section>
-    <footer class="footer">
-      <div class="content has-text-centered">
-        <p>
-          <strong>Bulma</strong> by
-          <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is
-          licensed
-          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The
-          website content is licensed
-          <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/"
-            >CC BY NC SA 4.0</a
-          >.
-        </p>
-      </div>
-    </footer>
+    <SluggoFooter />
   </UserProvider>
 </template>
 
@@ -25,17 +12,20 @@
 import { defineComponent } from "vue";
 import Navbar from "./components/Navbar";
 import UserProvider from "./components/UserProvider.vue";
+import SluggoFooter from "./components/SluggoFooter.vue";
 
 export default defineComponent({
   components: {
     Navbar,
-    UserProvider
+    UserProvider,
+    SluggoFooter
   }
 });
 </script>
 
 <style scoped>
 .is-fullheight {
+  /* this is kinda hacky. */
   min-height: calc(100vh - 72px - 168px);
 }
 </style>
---
+++ src/components/SluggoFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        <strong>Sluggo</strong>
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
